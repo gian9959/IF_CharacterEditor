5483a76afef6f6ff163aba8ca6a24f49e57bfb18
Gianmaria Vasino <gianmaria.vasino@edu.unito.it>
2025-03-17 15:41:54 +0100

    refactor and project setup

---
+++ if_editor_app/src/CEditor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CSS/App.css';
+
+class CEditor extends React.Component {
+  render() {
+    return (
+      <div className="Editor">
+        
+      </div>
+    );
+  }
+}
+
+export default CEditor;
---
+++ if_editor_app/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CSS/App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
