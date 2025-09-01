cc917505456d86b5a20916dea5fcedde61c7b131
Gianmaria Vasino <gianmaria.vasino@edu.unito.it>
2025-09-02 00:30:47 +0200

    markdown implementation

--- if_editor_app/src/Textbox.js
+++ if_editor_app/src/Textbox.js
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+import { marked } from 'marked'
+import DOMPurify from 'dompurify'
+
 function Textbox(props) {
     const position = {
     gridRowStart: props.row[0],
@@ -10,11 +14,34 @@ function Textbox(props) {
       props.updateFunc(props.id, value);
     }
 
+    const [md, setMD] = useState(false);
+
+    const updateMD = (btn) => {
+      let newMD = !md;
+      setMD(newMD);
+      if(newMD){
+        btn.classList.add('active');
+      } else {
+        btn.classList.remove('active');
+      }
+    }
+
+    marked.use({
+      breaks: true
+    });
+
     return (
         <div className='Textbox col' style={position}>
+          <div className='row'>
             <h2 className="whiteTitle">{props.title}</h2>
+            <button className='customButton mdButtons' onClick={(e) => updateMD(e.target)}>MD</button>
+          </div>
+          { !md &&
             <textarea className="textInput row" type="text" value={props.value} onChange={(e) => update(e.target.value)}>
-            </textarea>
+            </textarea>}
+          { md &&
+            <div className="mdInput" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(marked.parse(props.value))}}>
+            </div>}
         </div>
     );
 }
